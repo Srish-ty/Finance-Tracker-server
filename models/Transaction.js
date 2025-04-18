f488472ed4eb1fa2312af54826853932408768d9
Srishty Mangutte <srishtymangutte@gmail.com>
2025-04-18 18:38:54 +0530

    fix: remove versionkey in mongodb

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,32 +1,35 @@
 import mongoose from "mongoose";
 
-const transactionSchema = new mongoose.Schema({
-  amount: {
-    type: Number,
-    required: true,
+const transactionSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: [
+        "Groceries",
+        "Transport",
+        "Personal Care",
+        "Shopping",
+        "Saving or Investment",
+        "Healthcare",
+        "Entertainment",
+        "Other",
+      ],
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  category: {
-    type: String,
-    enum: [
-      "Groceries",
-      "Transport",
-      "Personal Care",
-      "Shopping",
-      "Saving or Investment",
-      "Healthcare",
-      "Entertainment",
-      "Other",
-    ],
-    required: true,
-  },
-});
+  { versionKey: false }
+);
 
 export default mongoose.model("Transaction", transactionSchema);
